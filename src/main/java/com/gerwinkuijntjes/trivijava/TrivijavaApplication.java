b3543c2acd8c111e5c2649c90823f6a28a87e176
Gerwin Kuijntjes <administratie@gerwinkuijntjes.nl>
2025-06-06 15:08:58 +0200

    Initial

---
+++ src/main/java/com/gerwinkuijntjes/trivijava/TrivijavaApplication.java
@@ -0,0 +1,13 @@
+package com.gerwinkuijntjes.trivijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrivijavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrivijavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gerwinkuijntjes/trivijava/TrivijavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerwinkuijntjes.trivijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrivijavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
